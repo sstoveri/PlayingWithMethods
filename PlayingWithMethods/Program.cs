dcfe244e895c411f13be1f15c14dde46046a906f
sstoveri <sstoveri45@gmail.com>
2022-03-14 09:37:14 -0500

    (Exercise 1) began user questions

---
+++ PlayingWithMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PlayingWithMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //---------------Exercise 1---------------
+            //Name: Sean
+            //Favorite Color: Blue
+            //Favorite animal: Wolf
+            //Favorite Band: Green Day
+
+            Console.WriteLine("Hello - What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName}. What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is an awesome color. What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Great! Now, what is your favorite band?");
+            var band = Console.ReadLine();
+        }
+    }
+}
