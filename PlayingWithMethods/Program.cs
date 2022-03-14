1b2c19ac9a3a24f78f58f392c1b01ea22b1e6513
sstoveri <sstoveri45@gmail.com>
2022-03-14 10:46:08 -0500

    (Exercise 2) sum and multiply

--- PlayingWithMethods/Program.cs
+++ PlayingWithMethods/Program.cs
@@ -6,31 +6,71 @@ namespace PlayingWithMethods
     {
         static void Main(string[] args)
         {
+
             //---------------Exercise 1---------------
             //Name: Sean
             //Favorite Color: Blue
             //Favorite animal: Wolf
             //Favorite Band: Green Day
 
-            Console.WriteLine("Hello - What is your first name?");
-            var userName = Console.ReadLine();
+            //Console.WriteLine("Hello - What is your first name?");
+            //var userName = Console.ReadLine();
 
-            Console.WriteLine($"Hi, {userName}. What is your favorite color?");
-            var color = Console.ReadLine();
+            //Console.WriteLine($"Hi, {userName}. What is your favorite color?");
+            //var color = Console.ReadLine();
 
-            Console.WriteLine($"{color} is an awesome color. What is your favorite animal?");
-            var animal = Console.ReadLine();
+            //Console.WriteLine($"{color} is an awesome color. What is your favorite animal?");
+            //var animal = Console.ReadLine();
 
-            Console.WriteLine("Great! Now, what is your favorite band?");
-            var band = Console.ReadLine();
+            //Console.WriteLine("Great! Now, what is your favorite band?");
+            //var band = Console.ReadLine();
 
-            Console.WriteLine($"Thanks, {userName}! Here is your profile");
-            Console.WriteLine("-----------------------");
+            //Console.WriteLine($"Thanks, {userName}! Here is your profile");
+            //Console.WriteLine("-----------------------");
 
-            Console.WriteLine($"Name: {userName}");
-            Console.WriteLine($"Favorite color: {color}");
-            Console.WriteLine($"Favorite animal: {animal}");
-            Console.WriteLine($"Favorite band: {band}");
+            //Console.WriteLine($"Name: {userName}");
+            //Console.WriteLine($"Favorite color: {color}");
+            //Console.WriteLine($"Favorite animal: {animal}");
+            //Console.WriteLine($"Favorite band: {band}");
+
+            //---------------Exercise 2---------------
+
+            Console.WriteLine("Give me a number to add");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to add to the first one");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = Sum(num1, num2);
+            Console.WriteLine($"The sum is: {sum}");
+
+            Console.WriteLine("Give me a number");
+            num1= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to multiply to the first one");
+            num2 = int.Parse(Console.ReadLine());
+
+            int product = Multiply(num1, num2);
+            Console.WriteLine($"The product is: {product}");
+        }
+
+        //---------------Exercise 2---------------
+        
+        public static int Sum(params int[] list)
+        {
+            int sum = 0;
+            for (int i = 0; i < list.Length; i++)
+            {
+                sum = sum + list[i];
+            }
+
+            return sum;
+        }
+      
+
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
         }
     }
 }
